d138fac95676e363bb8cc342391ecf601a1ab73a
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-20 18:34:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var p=document.querySelector("#pera");
+p.innerHTML="This pera is changed";
+// querySelector
+document.querySelector("a").innerHTML="home is changed";
+document.querySelector("li a").innerHTML="tutorial is changed";
+document.querySelector("div a").innerHTML="contact is changed";
+document.querySelector("#ban").innerHTML="i love bangladesh";
+// // querySelectorAll
+document.querySelectorAll("h2")[0].innerHTML="this heading 1 is changed";
+document.querySelectorAll("h2")[1].innerHTML="this heading 2 is changed";
